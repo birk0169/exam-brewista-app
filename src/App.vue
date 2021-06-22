b55069ce9afc0224be6d80baee23a222a58b825a
Birk Høgh-Mogensen <birk0169@edu.campusvejle.dk>
2021-06-22 14:56:35 +0200

    add animation/transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view/>
+  <router-view v-slot="slotProps"> 
+    <transition name="route">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <style>
@@ -8,5 +12,39 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: black;
+}
+
+.route-enter-active{
+  width: 100vw;
+  position: absolute;
+  animation: slide-in-alt 1s;
+}
+
+.route-leave-active{
+  width: 100vw;
+  position: absolute;
+  animation: slide-out-alt 1s;
+}
+
+@keyframes slide-in-alt{
+  0% {
+    transform: translateX(100vw);
+    opacity: 0;
+  }
+  100%{
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out-alt{
+  0% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  100%{
+    transform: translateX(-100vw);
+    opacity: 0;
+  }
 }
 </style>
